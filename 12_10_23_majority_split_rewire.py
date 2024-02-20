712083b60725462af13f468101e3ae810bc1c341
Nathan Bartley <bartleyn@uchicago.edu>
2024-02-20 12:07:03 -0800

    wrong majority split

--- 12_10_23_majority_split_rewire.py
+++ 12_10_23_majority_split_rewire.py
@@ -21,7 +21,7 @@ import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
-data_dir = ''
+data_dir = '/nas/home/nbartley/data/Twitter Bias/Network Data/2014-2015-twitter-seed/indiv_users'
 seeduser = pd.read_csv('{}/../seeduser.csv'.format(data_dir), header=0)
 seeduser['screen_name'] = seeduser['screen_name'].apply(lambda x: x[1:-1])
 
@@ -29,21 +29,45 @@ session_length = 30
 
 lngths = list(np.arange(10,150,10)) + [2000000000]
 split_done = False
-#all_new_popularity_3, all_new_random, all_new_revchron_2
+#all_new_popularity_3, all_new_random, all_new_revchron_full_july_actual, all_new_logit_full_july_actual
 #random_sample = np.random.choice([user for user in seeduser['user_id'].values], size=500, replace=False)
 random_sample = [user for user in seeduser['user_id'].values]
 
-kind = 'revchron'
+kind = 'logit'
 
 if not split_done:
-    with open('{}/gini_tuple_all_new_revchron_full_july_actual.pkl'.format(data_dir, kind), 'rb') as fp:##with open('{}/gini_tuple_all_new_popularity_3.pkl'.format(data_dir), 'rb') as fp:
+    with open('{}/gini_tuple_all_new_logit_full_july_actual.pkl'.format(data_dir, kind), 'rb') as fp:##with open('{}/gini_tuple_all_new_popularity_3.pkl'.format(data_dir), 'rb') as fp:
         gini_tup = pkl.load(fp)
     #random_sample = list(gini_tup[1].keys())#np.random.choice(list(gini_tup[1].keys()), replace=False, size=1000))
 
-if kind == 'popularity' or kind == 'random' or kind == 'revchron' or kind == 'logit' or kind == 'nnmf':
+print("done loading initial gini file")
+
+true_prevalence = 0.05
+
+
+'''
+if not split_done:
+    for user in random_sample: 
+        with open('{}/activity_dfs_{}_{}.pkl'.format(data_dir, kind, user), 'wb') as fp:
+            pkl.dump(gini_tup[1][user], fp)
+        
+    raise Exception('done with code')
+'''
+
+if kind == 'popularity' or kind == 'random' or kind == 'revchron' or kind == 'nnmf' or kind == 'logit':
+#    #attn_func = lambda edge: degrees_out[edge[0]]/degrees_in[edge[1]]
     attn_func = lambda edge: (degrees_out[edge[1]] / max(1, map_deg_friends[edge[0]])) if edge[1] in degrees_out and edge[0] in map_deg_friends else 0.0
+#    #attn_func = lambda edge:  degrees_out[edge[0]] / max(1,sum( [degrees_out[fr] if fr in degrees_out else 0.0 for fr in map_friends[edge[1]] if edge[1] in map_friends] ) if edge[1] in map_friends else 0.0)_ 
+#elif kind == 'correlated':
+#    attn_func = lambda edge:  degrees_out[edge[0]] / sum( degrees_out[fr] for fr in map_friends[edge[1]]]) if edge[1] in map_friends else 0.0 
 elif kind == 'random' or kind == 'revchron':# or kind=='popularity':
     attn_func = lambda edge: 1.0/degrees_in[edge[0]]
+#elif kind == 'friends-of-friends':
+#    attn_func = lambda edge: (1.0/sum( degrees_out[fr] for fr in map_friends[edge[1]]] +  degrees_out[y] for x in [q for q in map_friends[edge[1]] if q in map_friends] for y in map_friends[x]])) if edge[1] in map_friends else 0.0 
+#elif kind == 'correlated-corrected':
+#    attn_func = lambda edge:  degrees_out[edge[0]] / sum( degrees_out[fr] for fr in map_friends[edge[1]]])) if edge[1] in map_friends else 0.0 
+#elif kind == 'constant':
+#    attn_func = lambda edge: 1.0/degrees_in[edge[1]]
 
 
 try:
@@ -55,7 +79,12 @@ try:
         frac_dict = loaded_pkl[1]
         friends_dict = loaded_pkl[3]
         loaded_dicts = False
-
+    #with open('{}/frac_friend_dicts_2000_popularity.pkl'.format(data_dir), 'rb') as fp:
+    #    frac_friend_dicts_2000 = pkl.load(fp)
+    #with open('{}/frac_friend_dicts_2000_4000_popularity.pkl'.format(data_dir), 'rb') as fp:
+    #    frac_friend_dicts_2000_4000 = pkl.load(fp)   
+    #with open('{}/frac_friend_dicts_2000_popularity.pkl'.format(data_dir), 'rb') as fp:
+    #    frac_friend_dicts_4000 = pkl.load(fp)
 
 except Exception as e:
     pass
@@ -65,6 +94,7 @@ who_follows_who = pd.read_csv('{}/../usergraph.csv'.format(data_dir), header=0, 
 
 map_friends = who_follows_who[['id', 'tid']].groupby(['id', 'tid']).count().index.tolist()
 idx = pd.IndexSlice
+#map_friends = {tup[0]:[x[1] for x in map_friends if x[0] == tup[0]] for tup in map_friends}
 
 map_friends_2 = {}
 for tup in map_friends:
@@ -84,6 +114,23 @@ wfw_id = who_follows_who['id'].value_counts().index.tolist()
 wfw_tid = who_follows_who['tid'].value_counts().index.tolist()
 total_users = list(set(wfw_id + wfw_tid))
 
+# RANDOM ASSIGNMENT
+
+vals = rng.binomial(n=1, p=true_prevalence, size=len(total_users))
+user_map = {user:val for user, val in zip(total_users, vals)}
+
+
+map_friends_scores = {x:[user_map[y] for y in map_friends[x]] for x in map_friends}
+
+degrees_in = who_follows_who['id'].value_counts().to_dict()
+degrees_out = who_follows_who['tid'].value_counts().to_dict()
+G_in = degrees_in
+G_out = degrees_out
+degree_dist = [degrees_out[x] if x in degrees_out else 0 for x in user_map]
+assignments = [user_map[x] for x in user_map]
+
+avg_degree = np.mean(degree_dist)
+std_deg = np.std(degree_dist)
 
 act_dfs_dict = gini_tup[1]
 try:
@@ -95,20 +142,21 @@ except Exception as e:
         
         for act_df, length in zip(act_dfs_dict[user], lngths):
             friends_seen = np.array([x > 0 for x in act_df.sum(axis=1)])
-            edge_dict[length].extend([( user, fr) for fr, seen in zip(map_friends[user], friends_seen) if seen == True])
+            #self.edges_seen.add((tweet[0], user, float(in_deg(user)), float(self.user_map[user])))
+            edge_dict[length].extend([( user, fr, float(degrees_in[fr]) if fr in degrees_in else np.nan, float(user_map[fr])) for fr, seen in zip(map_friends[user], friends_seen) if seen == True])
     with open('{}/edge_dict_{}.pkl'.format(data_dir, kind), 'wb') as fp:
         pkl.dump(edge_dict, fp)
         
+'''
+# TOP FIVE PERCENT ASSIGNMENT
+first_five_pcnt = wfw_tid[:int(len(total_users)/20)]
 
-
-# RANDOM ASSIGNMENT
-
-true_prevalence = 0.05
-vals = rng.binomial(n=1, p=true_prevalence, size=len(total_users))
-user_map = {user:val for user, val in zip(total_users, vals)}
-
+user_map = {user:0 for user in total_users}
+user_map.update({user:1 for user in first_five_pcnt})
 
 map_friends_scores = {x:[user_map[y] for y in map_friends[x]] for x in map_friends}
+'''
+
 
 
 
@@ -139,22 +187,14 @@ def corr(x,y, um):
     print((p_pos / (std_deg * std_vals), np.abs(avg_pos_degree - avg_degree))) 
     return (p_pos / (std_deg * std_vals)) * np.abs(avg_pos_degree - avg_degree)
 
-degrees_in = who_follows_who['id'].value_counts().to_dict()
-degrees_out = who_follows_who['tid'].value_counts().to_dict()
-G_in = degrees_in
-G_out = degrees_out
-degree_dist = [degrees_out[x] if x in degrees_out else 0 for x in user_map]
-assignments = [user_map[x] for x in user_map]
 
-avg_degree = np.mean(degree_dist)
-std_deg = np.std(degree_dist)
 
 
 
 with joblib.parallel_backend(backend="loky"):
     if not loaded_days:
 
-        parallel = Parallel(verbose=100, n_jobs=14, temp_folder="/nas/home/nbartley/data/" )
+        parallel = Parallel(verbose=100, n_jobs=16, temp_folder="/nas/home/nbartley/data/" )
 
         folder = '{}/joblib_memmap'.format(data_dir)
         try:
@@ -162,6 +202,15 @@ with joblib.parallel_backend(backend="loky"):
         except FileExistsError:
             pass
 
+        #data_filename_memmap = os.path.join(folder, 'data_memmap')
+        #dump(gini_tup, data_filename_memmap)
+        #gini_tup= load(data_filename_memmap, mmap_mode='r')
+
+
+
+        #for ct_df, ct_df2, user in results:
+        #    ct_df.to_csv('{}/{}.csv'.format(data_dir, user))
+        #    ct_df2.to_csv('{}/{}_activity.csv'.format(data_dir, user))
         print("entering job")
 
         days_to_keep_dicts = parallel(delayed(job)(gini_tup[1][user], gini_tup[0][user], user) for user in random_sample)
@@ -172,17 +221,8 @@ if not loaded_days:
     for x in days_to_keep_dicts:
         days_to_keep_dict.update(x)
     
-def local_bias(edges, user_map, true_prev, day, G_in, G_out, attn_func = None):
-    in_deg = G_in
-    out_deg = G_out
-    if attn_func is None:
-        attn_func = lambda edge: (1.0/in_deg(edge[1])) if in_deg(edge[1]) > 0 else 0.0
-    num_edges = len(edges)
-    exp_val = 0.0
-    vals = map(lambda x: user_map[x[1]] * attn_func(x) / num_edges, edges)
-    exp_val = np.nansum(list(vals))
 
-    return np.mean(list(dict(in_deg).values())) * exp_val - true_prev
+
 
 
 
@@ -207,7 +247,10 @@ import time
 print(time.time())
 #print({x:user_map[x] for x in map_friends.loc[idx[12, :]].index.tolist()})
 def job(user_chunk):#(days_list, friends, act_dfs, map_friends, user_map, user):
+    #logger = logging.getLogger()
+    #logger.setLevel(logging.DEBUG)
 
+        #process = current_process()
     frac_dict = {}
     friends_dict = {}
     frac_tweets_dict = {}
@@ -228,6 +271,13 @@ def job(user_chunk):#(days_list, friends, act_dfs, map_friends, user_map, user):
         with open('{}/activity_dfs_{}_{}.pkl'.format(data_dir, kind, user), 'rb') as fp:
             act_dfs = pkl.load(fp)
 
+        #rng = np.random.default_rng()
+        #act_dfs = [df[rng.permutation(np.arange(df.shape[0])),:] for df in act_dfs]
+
+        #for df in act_dfs:
+        #    index = np.arange(df.shape[0])
+        #    np.random.shuffle(index)
+        #    df = df[index,:]
 
         friends_list = np.array(list(friends.keys()))
         
@@ -265,10 +315,27 @@ def job(user_chunk):#(days_list, friends, act_dfs, map_friends, user_map, user):
         
     
         #local_bias(edges, user_map, true_prev, day, G_in, G_out, attn_func = None)
+        '''
+        def local_bias(model, attn_func = None):
+    #self.edges_seen.add((tweet[0], user, float(in_deg(user)), float(self.user_map[user])))
+
+    edges = np.array(list(model.edges_seen))#np.array([[int(x) for x in edge.split('_')] for i, edge in enumerate(model.edges_seen)])
+    #np.array([(friend, user) for friend in model.edges_seen for user in model.edges_seen[friend]])
+
+    user_map = model.user_map
+    G_in = model.net.graph.in_degree
+    mean_in_deg = model.mean_in_deg
+    true_prev = model.true_prev
+    in_deg = G_in
+    num_edges = edges.shape[0]
         
+        '''
+        model = {"true_prevalence": true_prevalence, "edges_seen": edge_dict, "user_map": user_map}
+
         for length, act_df, act_df_bin in zip(list(np.arange(10,150,10))+[2000000000], act_dfs, [act_df.copy() for act_df in act_dfs]):
-            
-            local_bias_vals = local_bias(edge_dict[length], user_map, true_prevalence, None, G_in, G_out, attn_func)
+            local_bias_vals = au.local_bias(model, length, attn_func)
+
+            #local_bias_vals = au.local_bias(edge_dict[length], user_map, true_prevalence, None, G_in, G_out, attn_func)
 
             days_list = [x for x in range(act_df.shape[1])]#x for x in gini_dict[user][length]]# if len(x) >0]
             num_days = len(days_list)
@@ -319,6 +386,9 @@ def job(user_chunk):#(days_list, friends, act_dfs, map_friends, user_map, user):
                 bias_dict[user][str_len].extend(bias_vals.flatten().tolist())
                 continue
 
+            
+
+
 
 
             try:
@@ -341,6 +411,15 @@ def job(user_chunk):#(days_list, friends, act_dfs, map_friends, user_map, user):
             #frac_dict[user][str(length)] /= float(len(days_list)) if len(days_list) > 0 else 1
     return user_chunk, frac_dict, friends_dict, frac_tweets_dict, gini_dict, bias_dict
 
+'''
+try:
+    with open('{}/wfw_dict.pkl'.format(data_dir), 'rb') as fp:
+        wfw_dict = pkl.load(fp)
+except Exception as e:
+    wfw_dict = {user:who_follows_who[who_follows_who['id'] == user]['tid'].value_counts().index.tolist() for user in random_sample}
+    with open('{}/wfw_dict.pkl'.format(data_dir), 'wb') as fp:
+        pkl.dump(wfw_dict, fp)
+'''
 
 
 
@@ -371,7 +450,7 @@ def rewire(goal_corr):
 
     np_rand_choice = np.random.choice
     list_user_map_index = list_user_map.index
-    while (cur_corr < goal_corr) and (iters < 5000):
+    while (cur_corr < goal_corr) and (iters < 5000) and (delta > 1e-10):
 
         #es. For example, to increase ρkx, we randomly
         #choose nodes v1 with x = 1 and v0 with x = 0 and swap their attributes if the degree of v0 is
@@ -404,7 +483,27 @@ def rewire(goal_corr):
             delta = new_corr - cur_corr
             cur_corr = new_corr
         
-
+        '''
+        degree_pos = degree_dist[list_user_map_index(rand_pos)]
+        degree_neg = degree_dist[list_user_map_index(rand_neg)]
+        #print("selected neg node")
+        delta = 0.0
+        if degree_neg > degree_pos:
+            cur_user_map[rand_pos] = 0
+            cur_user_map[rand_neg] = 1
+            #positive_nodes.remove(rand_pos )
+            #negative_nodes.add(rand_pos)
+            #positive_nodes.add(rand_neg)
+            #negative_nodes.remove(rand_neg)
+            positive_nodes.pop(rand_pos)
+            positive_nodes[rand_neg] = 0
+            negative_nodes[rand_pos] = 0
+            negative_nodes.pop(rand_neg)
+            #cur_assignments = [cur_user_map[x] for x in cur_user_map]
+            new_corr = corr(degree_dist, [*cur_user_map.values()], [*positive_nodes])
+            delta = new_corr - cur_corr
+            cur_corr = new_corr
+        '''
         iters += 1
         
         print(cur_corr, delta, iters)
@@ -427,6 +526,11 @@ start = float(sys.argv[1])
 end = float(sys.argv[2])
 
 
+#for x in dir():
+#    print(x, sys.getrefcount(x))
+
+
+
 for goal_corr in np.arange(start, end, 0.15):#[0, 0.25, 0.50, 0.75, 1.0]:
     print("entering loop for correlation_kx ", goal_corr)
     assignments, user_map, cor = rewire(goal_corr)
@@ -444,13 +548,26 @@ for goal_corr in np.arange(start, end, 0.15):#[0, 0.25, 0.50, 0.75, 1.0]:
 
     
 
+    #data_filename_memmap = os.path.join(folder, 'usermap_memmap2')
+    #dump(user_map, data_filename_memmap)
+    #user_map = load(data_filename_memmap, mmap_mode='r')
+
+    #data_filename_memmap = os.path.join(folder, 'mapfriendsscores_memmap2')
+    #dump(map_friends_scores, data_filename_memmap)
+    #map_friends_scores = load(data_filename_memmap, mmap_mode='r')
+
+    #data_filename_memmap = os.path.join(folder, 'assignments_memmap')
+    #dump(assignments, data_filename_memmap)
+    #assignments = load(data_filename_memmap, mmap_mode='r')
+
+    #return user_chunk, frac_dict, friends_dict, frac_tweets_dict, gini_dict, bias_dict
     
     with joblib.parallel_backend(backend="loky"):
         if not loaded_dicts:
-            parallel = joblib.Parallel(verbose=100, n_jobs=14, temp_folder="/nas/home/nbartley/data/")#,require='sharedmem')
+            parallel = joblib.Parallel(verbose=100, n_jobs=10, temp_folder="/nas/home/nbartley/data/")#,require='sharedmem')
   
             
-            user_map_list = np.array_split(np.array(list(random_sample)), 25)
+            user_map_list = np.array_split(np.array(list(random_sample)), 80)
             #results = Parallel(n_jobs=4)(delayed(job)(user_chunk) for user_chunk in user_map_list)
             
             #frac_friend_dicts = parallel(delayed(job2)(random_sample[slicer:slicer+500]) for slicer in np.arange(0,1500,500))
@@ -459,7 +576,12 @@ for goal_corr in np.arange(start, end, 0.15):#[0, 0.25, 0.50, 0.75, 1.0]:
             frac_tweets_dict = {}
             gini_dict = {}
             bias_dict = {}
-            results = parallel(delayed(job)(user_chunk) for user_chunk in user_map_list)
+            results = parallel(delayed(job)(#[x for x in gini_dict[user][10] if len(x) >0], \
+                                                    #wfw_dict[user], \
+                                                    #gini_tup[1][user],\
+                                                    #map_friends,\
+                                                    # user_map,\
+                                                                                                user_chunk) for user_chunk in user_map_list)#, stats, seshs, lens in users_with_most_sessions)
             try:
                 for user_chnk, frac, frs, frac_tweets, gini, bias in results:
                     frac_dict.update(frac)
@@ -467,10 +589,10 @@ for goal_corr in np.arange(start, end, 0.15):#[0, 0.25, 0.50, 0.75, 1.0]:
                     frac_tweets_dict.update(frac_tweets)
                     gini_dict.update(gini)
                     bias_dict.update(bias)
-                with open('{}/frac_friend_dicts_{}_t5_{}_corrX_allusrs_gini_bias_weightedA_fixed_gini_actual.pkl'.format(data_dir,kind,  goal_corr), 'wb') as fp:
+                with open('{}/frac_friend_dicts_{}_t5_{}_corrX_allusrs_gini_bias_weightedA_fixed_gini_actual_12_10_23.pkl'.format(data_dir,kind,  goal_corr), 'wb') as fp:
                     pkl.dump((frac_dict, friends_dict, frac_tweets_dict, gini_dict, bias_dict), fp)  
             except ValueError as e:
                 print("screwed up unpacking ", e)
-                with open('{}/frac_friend_dicts_{}_t5_{}_corrX_allusrs_gini_bias_weightedA_fixed_gini_actual.pkl'.format(data_dir,kind,  goal_corr), 'wb') as fp:
+                with open('{}/frac_friend_dicts_{}_t5_{}_corrX_allusrs_gini_bias_weightedA_fixed_gini_actual_12_10_23.pkl'.format(data_dir,kind,  goal_corr), 'wb') as fp:
                     pkl.dump(results, fp)
               
